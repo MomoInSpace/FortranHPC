8a33a93a78adc557ae4b7df86e3a61e7f58f3494
MomoInSpace <unwissendweilmensch@gmx.de>
2024-02-02 14:15:10 +0100

    Fixing requirements: Fixing template path

--- docs/conf.py
+++ docs/conf.py
@@ -15,12 +15,12 @@ project_url = "."
 # -- Path setup --------------------------------------------------------------
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["/_templates"]
+templates_path = ["/docs/_templates"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["/_static"]
+html_static_path = ["/docs/_static"]
 
 # Final Path of css-files: _static/css/...
 html_css_files = ["/css/footer.css"]
